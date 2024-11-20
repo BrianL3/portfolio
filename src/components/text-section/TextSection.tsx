e3cde5f4bc502b510347b620b3db8cb4f3df9dd0
Brian Ledbetter <brian.ledbetter@gettyimages.com>
2024-11-20 14:36:15 -0800

    Fixing up heroes, progressing UI

--- src/components/text-section/TextSection.tsx
+++ src/components/text-section/TextSection.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 import './TextSection.css';
+import useMediaQuery from '../../util/MediaQuery';
+import { screenWidths } from '../../util/MediaBreakpoints';
 
 interface TextSectionProps {
   title: string;
@@ -15,6 +17,8 @@ const TextSection: FC<TextSectionProps> = ({
   title,
   textblocks
 }: TextSectionProps) => {
+  const isLargeScreen = useMediaQuery(`(min-width: ${screenWidths.tablet})`);
+
   return (
     <section
       id="locations"
@@ -24,6 +28,11 @@ const TextSection: FC<TextSectionProps> = ({
       <h2 className="mzp-u-title-md" style={{ textAlign: 'center' }}>
         {title}
       </h2>
+      <img
+          src={import.meta.env.BASE_URL+ "imgs/hero-image.jpg" }
+          style={{maxHeight: isLargeScreen ? 300 : 270, objectFit:'contain', borderRadius: 12 }}
+          alt={"a picture of brian, the author of this website"}
+        />
       <div style={{height:30}}/>
       {textblocks.map((section, index) => (
         <div key={index} className="textsection-card" style={{backgroundColor: '-moz-initial'}}>
--- src/data/PublishedWork.ts
+++ src/data/PublishedWork.ts
@@ -114,42 +114,42 @@ const cdp = [
 
 const publishedWorks = [{
   linkUrl: "istock-app",
-  imageUrl: import.meta.env.BASE_URL+"/istock-app/app-icon.png",
+  imageUrl: import.meta.env.BASE_URL+"istock-app/app-icon.png",
   imageDescription: "iStock's app icon",
   title: "iStock by Getty",
   detailText: "An asset storefront application for iStock, a property of Getty Images. You can search the entire iStock image collection based on any criteria you can dream of, including looking for images similar to a picture you upload. See larger images in full-screen view with rotation and pinch-and-zoom. Save images you like to Boards, and share them via email and messaging."
 },
 {
   linkUrl: "releases-app",
-  imageUrl: import.meta.env.BASE_URL+"/releases-app/app-icon.png",
+  imageUrl: import.meta.env.BASE_URL+"releases-app/app-icon.png",
   imageDescription: 'ios releases app icon',
   title: "Releases",
   detailText: 'Getty Images Releases is a free all-in-one digital release management app for Getty Images and iStock creators that lets you create, complete, and manage all of your model and property releases on your phone. Releases enables creators to adopt a 100% digital release workflow. Create a new model or property release, have the model or property agent complete and sign it right there on your device.'
 },
 {
   linkUrl: "getty-app",
-  imageUrl: import.meta.env.BASE_URL+"/getty-ios-app/getty-ios-icon.png",
+  imageUrl: import.meta.env.BASE_URL+"getty-ios-app/getty-ios-icon.png",
   imageDescription: 'getty images ios storefront app icon',
   title: "Getty on iOS",
   detailText: 'Designed for creative and media professionals, and photography enthusiasts everywhere. Search, save and share our spectacular collection of creative, editorial and archival imagery and video — wherever and whenever you want.'
 },
 {
   linkUrl: "test-link",
-  imageUrl: import.meta.env.BASE_URL+"/imgs/android_robot.png",
+  imageUrl: import.meta.env.BASE_URL+"imgs/android_robot.png",
   imageDescription: 'test-image-description',
   title: "Getty/iStock Android",
   detailText: 'The asset storefront for Android users. They can seamlessly browse and download their purchased assets, while also organizing their favorites into customizable boards for easy access later. This app was written in Kotlin with flavors: one for Getty and one for iStock. It offers the same intuitive interface for both brands, ensuring that users can manage their digital assets effortlessly.'
 },
 {
   linkUrl: "cdp",
-  imageUrl: import.meta.env.BASE_URL+"/imgs/cdp-icon.png",
+  imageUrl: import.meta.env.BASE_URL+"imgs/cdp-icon.png",
   imageDescription: 'test-image-description',
   title: "Council Data Project",
   detailText: 'An open-source search engine for local government. We aim to empower journalists, activists, and community members in following council action by combining and simplifying sources of information on municipal council meetings and actions.'
 },
 {
   linkUrl: "test-link",
-  imageUrl: import.meta.env.BASE_URL+"/imgs/identity-icon.png",
+  imageUrl: import.meta.env.BASE_URL+"imgs/identity-icon.png",
   imageDescription: 'portfolio site icon',
   title: "My Portfolio",
   detailText: " This portfolio site, built with React and Vite, showcases my work and skills as a web developer. Explore my projects, learn about my journey, and see how I approach problem-solving in the tech landscape. The source code is available on GitHub for those interested in the development process!"
